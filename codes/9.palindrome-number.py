1c4e6636f8943fd654ec5d6c2be15e2d84e09244
yatoyun <kaneko.yuta0813@gmail.com>
2023-07-06 21:14:24 +0900

    easy

---
+++ codes/9.palindrome-number.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=9 lang=python3
+#
+# [9] Palindrome Number
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # minus number is absolutely not palindrome
+        if x < 0: return False
+
+        return str(x) == str(x)[::-1]
+        
+# @lc code=end
+
